f526bd135b50611c0ba79b10f613c6a95b5021e7
Itay Volk <itay.volk1@gmail.com>
2024-12-01 01:11:06 -0800

    Day 1
    Solved day 1

---
+++ day 1/part1.c
@@ -0,0 +1,36 @@
+/* 
+   Author: Itay Volk
+   Date: 12/01/2024
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+
+#define NUM 1000
+
+int compare(const void* a, const void* b) {
+   return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int *list_a = calloc(NUM, sizeof(int));
+    int *list_b = calloc(NUM, sizeof(int));
+    FILE * fp = fopen("data.txt", "r");
+    char line[256];
+    for (int i = 0; i < NUM; i++) {
+        fgets(line, sizeof(line), fp);
+        list_a[i] = atoi(strtok(line, " "));
+        list_b[i] = atoi(strtok(NULL, " "));
+    }
+    qsort(list_a, NUM, sizeof(int), compare);
+    qsort(list_b, NUM, sizeof(int), compare);
+    int dist = 0;
+    for (int i = 0; i < NUM; i++) {
+        dist += abs(list_a[i]-list_b[i]);
+    }
+    printf("%d\n", dist);
+    return 0;
+}
---
+++ day 1/part2.c
@@ -0,0 +1,40 @@
+/* 
+   Author: Itay Volk
+   Date: 12/01/2024
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+
+#define NUM 1000
+
+int compare(const void* a, const void* b) {
+   return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int *list_a = calloc(NUM, sizeof(int));
+    int *list_b = calloc(NUM, sizeof(int));
+    FILE * fp = fopen("data.txt", "r");
+    char line[256];
+    for (int i = 0; i < NUM; i++) {
+        fgets(line, sizeof(line), fp);
+        list_a[i] = atoi(strtok(line, " "));
+        list_b[i] = atoi(strtok(NULL, " "));
+    }
+    int siml = 0;
+    for (int i = 0; i < NUM; i++) {
+        int count = 0;
+        for (int j = 0; j < NUM; j++) {
+            if (list_a[i] == list_b[j]) {
+                count++;
+            }
+        }
+        siml += list_a[i]*count;
+    }
+    printf("%d\n", siml);
+    return 0;
+}
